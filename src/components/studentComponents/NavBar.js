e8ac86e4102bb79e4805ea11808c0f1978bb73d1
Arij ELKorbi <arij.korbi@ensi-uma.tn>
2021-03-22 15:58:13 +0100

    first commit

---
+++ src/components/studentComponents/NavBar.js
@@ -0,0 +1,36 @@
+import React from 'react'
+
+function NavBar() {
+    return (
+       
+        
+         
+    <>
+  <header id="header" className="fixed-top d-flex align-items-center">
+    <div className="container">
+      <div className="header-container d-flex align-items-center justify-content-between">
+        <div className="logo">
+          <h1 className="text-light"><a href="index.html"><span>E-learning</span></a></h1>
+          </div>
+        <nav id="navbar" className="navbar">
+          <ul>
+            <li><a className="nav-link scrollto active" href="#hero">Home</a></li>
+            <li><a className="nav-link scrollto" href="#about">About</a></li>
+            <li><a className="nav-link scrollto" href="#services">Courses</a></li>
+        
+            <li><a className="nav-link scrollto" href="#contact">Search</a></li>
+
+            <li><a className="nav-link scrollto" href="#contact">Contact</a></li>
+            <li><a className="getstarted scrollto" href="#about">Sign in</a></li>
+          </ul>
+          <i className="bi bi-list mobile-nav-toggle" />
+        </nav>
+      </div>
+    </div>
+  </header>
+  
+      </>  
+    )
+}
+
+export default NavBar
---
+++ src/pages/Home.js
@@ -0,0 +1,28 @@
+import React from 'react'
+// import NavBar from '../components/studentComponents/NavBar'
+// import Footer from '../components/studentComponents/Footer'
+// import Cover from '../components/studentComponents/Cover'
+// import Courses from '../components/studentComponents/Courses'
+// import CountSection from '../components/studentComponents/CountSection'
+// import About from '../components/studentComponents/About'
+// import Recommended from '../components/studentComponents/Recommended'
+import Aa from '../components/studentComponents/Aa'
+
+function Home() {
+    return (
+        <>
+         {/* <NavBar/>
+        <Cover/>
+        <About/>
+         <CountSection/>
+         <Recommended/>
+         <Courses/>
+    
+
+        <Footer/> */}
+    <Aa/> 
+        </>
+    )
+}
+
+export default Home
